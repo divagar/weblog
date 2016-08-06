0db838ef68950452ae90e2895c4c959f2765da82
Div <m.divagar@gmail.com>
2016-08-06 21:01:42 +0530

    Dasboard page - initial layout/design and login functionlity.
    Signed-off-by: Div <m.divagar@gmail.com>

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,17 +1,67 @@
-import { Component, OnInit } from '@angular/core';
-@Component ({
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
+import { Routes, Router, RouteSegment, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router';
+import { LoginComponent } from './login.component';
+import { Observable } from 'rxjs/RX';
+import { Title } from '@angular/platform-browser';
+
+declare var jQuery: any;
+declare var tinymce: any;
+
+@Component({
     moduleId: module.id,
     selector: 'dashboard',
     templateUrl: 'dashboard.component.html',
-    styleUrls: ['dashboard.component.css']
+    styleUrls: ['dashboard.component.css'],
+    providers: [LoginComponent]
 })
-export class DashboardComponent implements OnInit {
 
-  constructor() {}
+export class DashboardComponent implements OnInit, AfterViewInit {
 
-  ngOnInit() {}
+    dAlert: boolean;
+    dAlertMsg: string;
+    dAlertType: string;
 
+    constructor(public af: AngularFire,
+        public loginUser: LoginComponent,
+        public router: Router,
+        private titleService: Title) {
+
+        //Set page title
+        this.titleService.setTitle("Weblog | Dashboard");
+
+        //get af auth status
+        af.auth
+            .do(v => this.dashboardLogin(v))
+            .subscribe(user => this.dashboardLogin(user))
+    }
+
+    ngOnInit() {}
+
+    ngAfterViewInit() {}
+
+    dashboardLogin(user) {
+        //check user credentials
+        this.loginUser.userCredentials(user);
+        if (this.loginUser.user == null)
+            this.router.navigate(['/login']);
+        //else
+            //selectCategory
+            //this.getCategory();
+    }
+
+    dashboardLogout() {
+        this.loginUser.logoutUser();
+    }
+
+
+    dashboardalert(type, msg, status) {
+        this.dAlertType = type;
+        this.dAlertMsg = msg;
+        this.dAlert = status;
+    }
+
+    closeAlert() {
+        this.dAlert = false;
+    }
 }
-
-
-
