617fec81d06e1e580174e23c59f7ddc5ceea7ee2
Div <m.divagar@gmail.com>
2016-08-06 19:53:53 +0530

    Login page - layout/design and flow
    Signed-off-by: Div <m.divagar@gmail.com>

--- src/app/login.component.ts
+++ src/app/login.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Routes, Router, RouteSegment, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router';
+import {
+  AuthMethods,
+  AuthProviders,
+  defaultFirebase,
+  AngularFire,
+  FIREBASE_PROVIDERS,
+  FirebaseListObservable,
+  FirebaseAuthState,
+  FirebaseApp} from 'angularfire2';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/do';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   moduleId: module.id,
@@ -6,9 +19,69 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: 'login.component.html',
   styleUrls: ['login.component.css']
 })
-export class LoginComponent implements OnInit {
 
-  constructor() {}
-  ngOnInit() {}
+export class LoginComponent {
+
+  loginAlert: boolean;
+  loginAlertMsg: string;
+  loginAlertType: string;
+
+  user: FirebaseAuthState;
+
+  constructor(
+    public af: AngularFire,
+    public router: Router,
+    private titleService: Title) {
+
+    //Set page title
+    this.titleService.setTitle("Weblog | Login");
+
+    //get af auth status
+    af.auth
+      .do(v => this.userCredentials(v))
+      .subscribe(user => this.userCredentials(user))
+  }
+
+  /* login user */
+  loginUser(email: string, password: string) {
+    this.af.auth.login({ email, password }, {
+      method: AuthMethods.Password,
+      provider: AuthProviders.Password
+    })
+      .then((user) => this.loginSuccess(user))
+      .catch(e => this.loginFailed(e));
+  }
+  logoutUser() {
+    this.af.auth.logout();
+    this.router.navigate(['/login']);
+  }
+  loginSuccess(user) {
+    this.loginalert('info', 'Login successfull.', true);
+    this.checkCredentials();
+  }
+  loginFailed(e) {
+    this.loginalert('danger', 'Login ' + e, true);
+  }
+
+  userCredentials(user) {
+    this.user = user;
+  }
+  checkCredentials() {
+    console.log(this.user);
+    if (this.user != null)
+      this.router.navigate(['/dashboard']);
+    else
+      this.router.navigate(['/login']);
+  }
+
+  /* Alert */
+  loginalert(type, msg, status) {
+    this.loginAlertType = type;
+    this.loginAlertMsg = msg;
+    this.loginAlert = status;
+  }
+  closeAlert() {
+    this.loginAlert = false;
+  }
 
 }
