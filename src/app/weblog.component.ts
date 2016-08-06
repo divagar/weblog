6bc36ec55722c6c8317f1f096f476f34c337c1de
Div <m.divagar@gmail.com>
2016-08-06 16:36:41 +0530

    Basic app layout completed.
    This change includes:
    - Basic layout.
    - Page routing.
    - Page bg added.
    - forked a template from start bootstrap.
    - placeholder text are added.

--- src/app/weblog.component.ts
+++ src/app/weblog.component.ts
@@ -1,11 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HomeComponent } from './home.component';
+import { LoginComponent } from './login.component';
+import { DashboardComponent } from './dashboard.component';
+import { PostComponent } from './post.component';
+import { AboutComponent } from './about.component';
+import { ContactComponent } from './contact.component';
+import { Routes, Router, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router';
 
 @Component({
   moduleId: module.id,
-  selector: 'weblog-app',
+  selector: 'weblog',
   templateUrl: 'weblog.component.html',
-  styleUrls: ['weblog.component.css']
+  styleUrls: ['weblog.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS]
 })
-export class WeblogAppComponent {
-  title = 'C o m i n g  S o o n';
+@Routes([
+  {
+    path: '/login', component: LoginComponent
+  },
+  {
+    path: '/dashboard', component: DashboardComponent
+  },
+  {
+    path: '/post', component: PostComponent
+  },
+  {
+    path: '/post/*', component: PostComponent
+  },
+  {
+    path: '/about', component: AboutComponent
+  },
+  {
+    path: '/contact', component: ContactComponent
+  },
+  {
+    path: '/home', component: HomeComponent
+  },
+  {
+    path: '/', component: HomeComponent
+  },
+  {
+    path: '*', component: HomeComponent
+  }
+])
+
+export class WeblogAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  title = 'W e l c o m e';
 }
